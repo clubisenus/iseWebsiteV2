a060f916b101e7209dee0e59fe360af8c1e02e9d
Mooncheez <62787982+johngao122@users.noreply.github.com>
2025-07-14 22:17:09 +0800

    aldi is gone

--- src/pages/MC18.js
+++ src/pages/MC18.js
@@ -1,58 +1,72 @@
-import React from 'react';
-import { compose } from 'redux';
-import { connect } from 'react-redux';
-import { withRouter } from 'react-router-dom';
-import { withTheme } from '@material-ui/styles';
+import React from "react";
+import { compose } from "redux";
+import { connect } from "react-redux";
+import { withRouter } from "react-router-dom";
+import { withTheme } from "@material-ui/styles";
 
-import TopBar from '../components/TopBar';
-import LeftDrawer from '../components/LeftDrawer';
-import { writeups } from '../resources/committees';
-import committee from '../resources/images/committee.jpg';
+import TopBar from "../components/TopBar";
+import LeftDrawer from "../components/LeftDrawer";
+import { writeups } from "../resources/committees";
+import committee from "../resources/images/committee.JPG";
 
 import {
-  pic01, pic02, pic11, mc18banner, p18, vpi18, vpe18, gs18, fs18,
-  isedpd18, ncpd18, slpdc18, slpdo18, wpd18, baccpdc18, baccpdt18
-} from '../resources/images';
+    pic01,
+    pic02,
+    pic11,
+    mc18banner,
+    p18,
+    vpi18,
+    vpe18,
+    gs18,
+    fs18,
+    isedpd18,
+    ncpd18,
+    slpdc18,
+    slpdo18,
+    wpd18,
+    baccpdc18,
+    baccpdt18,
+} from "../resources/images";
 
-const mapStateToProps = state => {
-  return {
-    //state:state
-  };
+const mapStateToProps = (state) => {
+    return {
+        //state:state
+    };
 };
 
-const mapDispatchToProps = dispatch => {
-  return {};
+const mapDispatchToProps = (dispatch) => {
+    return {};
 };
 
 const styles = {
-  standardizedImage: {
-    width: '100%',
-    height: 'auto',
-    objectFit: 'cover',
-    borderRadius: '8px',
-    margin: '10px 0',
-  },
-  container: {
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    padding: '10px',
-    textAlign: 'center',
-  },
-  writeup: {
-    width: '100%',
-    maxWidth: '100%', 
-    boxSizing: 'border-box',
-  },
-  header: {
-    textAlign: 'center',
-    marginBottom: '20px',
-  },
-  mainImage: {
-    width: '100%',
-    height: 'auto',
-  },
-  mediaQueries: `
+    standardizedImage: {
+        width: "100%",
+        height: "auto",
+        objectFit: "cover",
+        borderRadius: "8px",
+        margin: "10px 0",
+    },
+    container: {
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        padding: "10px",
+        textAlign: "center",
+    },
+    writeup: {
+        width: "100%",
+        maxWidth: "100%",
+        boxSizing: "border-box",
+    },
+    header: {
+        textAlign: "center",
+        marginBottom: "20px",
+    },
+    mainImage: {
+        width: "100%",
+        height: "auto",
+    },
+    mediaQueries: `
     @media (max-width: 768px) {
       .main {
         padding: 10px;
@@ -103,45 +117,44 @@ const styles = {
 };
 
 const MC18 = (props) => (
-  <div>
-    <style>{styles.mediaQueries}</style>
-    <LeftDrawer display={props.display} />
-    <TopBar />
-    <div id="main" style={{ width: '100%' }}>
-      <div className="inner">
-        <section>
-          <header className="main" style={styles.header}>
-            <h1>23rd Management Committee</h1>
-          </header>
-          <span className="image main"><img src={committee} alt="" style={styles.mainImage} /></span>
-          <div style={styles.container}>
-            {writeups.map((writeup) => (
-              <div key={writeup.position} style={styles.writeup}>
-                <hr className="major" />
-                <h2>{writeup.position}</h2>
-                <p>
-                  <span className={writeup.imageposition}>
-                    <img 
-                      src={writeup.image} 
-                      alt="" 
-                      style={styles.standardizedImage}
-                    />
-                  </span>
-                  <strong>{writeup.name}</strong> {writeup.text}
-                </p>
-              </div>
-            ))}
-          </div>
-        </section>
-      </div>
+    <div>
+        <style>{styles.mediaQueries}</style>
+        <LeftDrawer display={props.display} />
+        <TopBar />
+        <div id="main" style={{ width: "100%" }}>
+            <div className="inner">
+                <section>
+                    <header className="main" style={styles.header}>
+                        <h1>23rd Management Committee</h1>
+                    </header>
+                    <span className="image main">
+                        <img src={committee} alt="" style={styles.mainImage} />
+                    </span>
+                    <div style={styles.container}>
+                        {writeups.map((writeup) => (
+                            <div key={writeup.position} style={styles.writeup}>
+                                <hr className="major" />
+                                <h2>{writeup.position}</h2>
+                                <p>
+                                    <span className={writeup.imageposition}>
+                                        <img
+                                            src={writeup.image}
+                                            alt=""
+                                            style={styles.standardizedImage}
+                                        />
+                                    </span>
+                                    <strong>{writeup.name}</strong>{" "}
+                                    {writeup.text}
+                                </p>
+                            </div>
+                        ))}
+                    </div>
+                </section>
+            </div>
+        </div>
     </div>
-  </div>
 );
 
 export default withRouter(
-  withTheme(
-    compose(
-      connect(mapStateToProps, mapDispatchToProps)
-    )(MC18)
-  )
+    withTheme(compose(connect(mapStateToProps, mapDispatchToProps))(MC18))
 );
